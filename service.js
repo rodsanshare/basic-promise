372bdf24ba2f5d0a7de99612890ca28258c37b7c
Rodney <rodsan.724@outlook.com>
2018-06-13 22:57:06 -0400

    using let and return promise

--- service.js
+++ service.js
@@ -1,16 +1,20 @@
 exports.test = function (x) {
-    return new Promise(function (resolve, reject) {
+    let promise = new Promise(function (resolve, reject) {
         setTimeout(() => {
             if (x === 1)
                 resolve('hey2');
             else
                 reject(new Error('Error has occurred.'));
         }, 3000);
-    }).then(function (word) {
-        console.log(word);
-        return word;
-    }).catch(function (err) {
-        console.log(err);
-    })
+    });
 
-}
+    return promise
+        .then(function (word) {
+            console.log(word);
+            return word;
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+
+};

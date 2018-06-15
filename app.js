c819382bfe0acbf0774da7dd64cd416c39dc0131
Rodney <rodsan.724@outlook.com>
2018-06-14 21:51:07 -0400

    full CRUD for contracts!

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const contracts = require('./service');
 
 (async () => {
-    var results = await contracts.test(1) //Play around with this parameter
+    var results = await contracts.test(1); //Play around with this parameter
     console.log(results);
 
-    var results2 = await contracts.get() //Play around with this parameter
+    var results2 = await contracts.get(); //Play around with this parameter
     console.log(results2);
+
+    var results3 = await contracts.update('KATIE-SCARLET', 2); //Play around with this parameter
+    console.log(results3);
+
+    var results4 = await contracts.insert('THOMAS-JAMES', Math.random()); //Play around with this parameter
+    console.log(results4.split(',')[1] + ' was inserted.');
+
+    var results5 = await contracts.getOne(results4.split(',')[0]);
+    console.log(`${results5[0].firstName}  was retrieved with id of ${results5[0].id}.`);
+
+    var results6 = await contracts.delete(results5[0].id); //Play around with this parameter
+    console.log(results6);
 })();
--- service.js
+++ service.js
@@ -17,6 +17,48 @@ exports.get = function () {
     });
 };
 
+exports.getOne = function (id) {
+    return new Promise(function (resolve, reject) {
+        con.query("SELECT * FROM " + process.env.DATABASE + ".contracts WHERE id=?", [id], function (err, result, fields) {
+            if (err) throw err;
+            resolve(result);
+        });
+    });
+};
+
+exports.delete = function (id) {
+    return new Promise(function (resolve, reject) {
+        con.query("DELETE FROM " + process.env.DATABASE +
+            ".contracts WHERE id=?", [id],
+            function (err, result) {
+                if (err) throw err;
+                resolve(result.affectedRows + ' row(s) deleted.');
+            });
+    });
+};
+
+exports.update = function (name, id) {
+    return new Promise(function (resolve, reject) {
+        con.query("UPDATE " + process.env.DATABASE +
+            ".contracts SET firstName=? WHERE id=?", [name, id],
+            function (err, result) {
+                if (err) throw err;
+                resolve(result.affectedRows + ' ' + name);
+            });
+    });
+};
+
+exports.insert = function (firstName, vinId, ) {
+    return new Promise(function (resolve, reject) {
+        con.query("INSERT INTO " + process.env.DATABASE +
+            ".contracts (firstName, vehicleId) VALUES (?,?)", [firstName, vinId],
+            function (err, result) {
+                if (err) throw err;
+                resolve(result.insertId + ',' + result.affectedRows + ' ' + firstName);
+            });
+    });
+};
+
 exports.test = function (x) {
     return new Promise(function (resolve, reject) {
         setTimeout(() => {
